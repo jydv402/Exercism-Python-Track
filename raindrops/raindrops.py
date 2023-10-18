2ca09504b5a2288e78173ef4c660666b2e812048
jydv402 <jydv402@gmail.com>
2023-10-18 19:40:24 +0530

    Completed raindrops

---
+++ raindrops/raindrops.py
@@ -0,0 +1,16 @@
+def convert(number):
+
+    result = ""
+    if number % 3 == 0:
+        result += "Pling"
+
+    if number % 5 == 0:
+        result += "Plang"
+
+    if number % 7 == 0:
+        result += "Plong"
+
+    if not(number % 3 == 0 or number % 5 == 0 or number % 7 == 0):
+        return str(number)
+
+    return result
